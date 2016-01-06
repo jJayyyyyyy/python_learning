2c87f78d2a2d68c4693726d9c9bb7a1e5b39f820
Steve <ljjdbd123@hotmail.com>
2016-01-06 22:12:57 +0800

    add <dict_set.py> as an example <dictionary, set>

---
+++ dict_set.py
@@ -0,0 +1,31 @@
+dic = {'Michael':96, 'Bob':70}
+print('Bob')
+
+dic['Adam'] = 67
+print(dic['Adam'])
+
+#name = input('input name: ')
+#if name in dic:
+#	print(dic[name])
+
+print('Thoms' in dic)
+
+#print(dic.get('Thom', -1))
+
+s = set([2, 2, 2, 1, 3, 4])
+print(s)
+#add
+s.add(4)
+print(s)
+s.add(5)
+print(s)
+#delete
+s.pop()
+print(s)
+s.remove(4)
+print(s)
+
+t1 = (1, 2, 3)
+s1 = set(t1)
+print(s1)
+
