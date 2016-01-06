485f0201965acad5eb837e4790f4add4537a26f8
Steve <ljjdbd123@hotmail.com>
2016-01-06 10:54:26 +0800

    add <for_while.py> as an example of <Loop Construct>

---
+++ code/for_while.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+names = ['Micheal','Bob','Tracy']
+for name in names:
+	print (name)
+
+for name in names:
+	print ('Hello, %s!' % name)
+
+sum = 0
+for x in [1, 2, 3]:
+	sum += x
+print(sum)
+
+a = list(range(5))
+print(a)
+
+n = 1
+sum = 0
+while n<=99:
+	sum += n
+	n += 2
+print(sum)
+
+sum=0;
+for i in range(1, 101):
+	sum += i
+print(sum)
+
