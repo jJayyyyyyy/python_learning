1e462e1d1b7b0a563b6e85fd68d18e96ccfdcc8b
Steve <ljjdbd123@hotmail.com>
2016-01-07 14:19:41 +0800

    add func_call.py

---
+++ func_call.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+abs(-3)
+max(1, 2, 5, 3, 1)
+
+int('123')
+int(12.34)
+float(12)
+float('12.3')
+str(123)
+str(1.2)
+bool(1)
+bool('1')
+bool('')
+bool('0')
+bool(0)
+
+
+a = abs
+a(-1)
+
+n1=255
+n2=1000
+print(hex(n1))
+print(hex(n2))
