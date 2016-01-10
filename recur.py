0f8956fc1e49b1a201aa7ff8b330ee7a1e0354fd
Steve <ljjdbd123@hotmail.com>
2016-01-10 10:59:57 +0800

    update <recur.py>

--- recur.py
+++ recur.py
@@ -13,3 +13,14 @@ print(fact(3))
 #在计算机中，函数调用是通过栈（stack）这种数据结构实现的，
 #每当进入一个函数调用，栈就会加一层栈帧，每当函数返回，栈就会减一层栈帧。
 #由于栈的大小不是无限的，所以，递归调用的次数过多，会导致栈溢出。
+
+#优化
+#尾递归是指，在函数返回的时候，调用函数本身，并且，return语句不能包含表达式。
+#这样，编译器或者解释器就可以把尾递归做优化
+#使递归本身无论调用多少次，都只占用一个栈帧，不会出现栈溢出的情况。
+def fact_pro(n):
+	return fact_iter(n, 1)
+def fact_iter(num, product):
+	if num==1:
+		return product
+	return fact_iter(num-1, num*product)
